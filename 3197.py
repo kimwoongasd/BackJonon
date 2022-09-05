0b1fcfde1db4f6b7fdd255cd2b86920462fa23cf
woong <dndvud123@naver.com>
2022-09-05 21:07:16 +0900

    can't solve the problem

--- 3197.py
+++ 3197.py
@@ -1,25 +1,31 @@
+from collections import deque
+
 r, c = map(int, input().split())
 
 dx = [-1, 0, 1, 0]
 dy = [0, 1, 0, -1]
 
-def dfs():
+def dfs(x, y):
     pass
 
-ch = []
-swan = [[0 for i in range(c)] for j in range(r)]
-ice = [[0 for i in range(c)] for j in range(r)]
+ice = []
+swan = [[0 for _ in range(c)] for _ in range(r)]
+ch = [[0 for _ in range(c)] for _ in range(r)]
 
 for i in range(r):
     
     tem = list(input().rstrip())
-    ch.append(tem)
+    ice.append(tem)
     
     for j in range(c):
         if tem[j] == "X":
             swan[i][j] = 1
         elif tem[j] == "L":
             swan[i][j] = 2
-    
+
+for i in range(r):
+    for j in range(c):
+        pass
+
 for x in swan:
     print(x)
