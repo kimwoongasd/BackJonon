8bdb443253b54990ddf6db95ae5fd95785bb5343
woong <dndvud123@naver.com>
2022-09-05 21:46:15 +0900

    solve the problem 2525

--- 2525.py
+++ 2525.py
@@ -1,14 +1,13 @@
 a, b = map(int, input().split())
 c = int(input())
 
-m = 0
 total = b + c
 mod = total % 60
 
 if total >= 60:
     a += total // 60
     
-    if a >= 24:
-        a = 0
+if a >= 24:
+    a -= 24
 
 print(f"{a} {mod}")
