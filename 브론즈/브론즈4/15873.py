813bfaf97a0425c7af7e321ad864e43af92ff8a6
woong <dndvud123@naver.com>
2022-09-22 01:30:25 +0900

    solve the problem

--- 브론즈/브론즈4/15873.py
+++ 브론즈/브론즈4/15873.py
@@ -1,14 +1,24 @@
 n = input()
 
-res = 0
-tem = 0
-for x in n:
-    if int(x) == 0:
-        res -= tem
-        tem *=10
-        res += tem
+# res = 0
+# tem = 0
+# for x in n:
+#     if int(x) == 0:
+#         res -= tem
+#         tem *=10
+#         res += tem
         
-    tem = int(x)
-    res += int(x)
+#     tem = int(x)
+#     res += int(x)
     
-print(res)
+# print(res)
+
+if len(n) == 4:
+    print(20)
+elif len(n) == 2:
+    print(int(n[0]) + int(n[1]))
+else:
+    if n[1] == "0":
+        print(10 + int(n[2]))
+    else:
+        print(10 + int(n[0]))
