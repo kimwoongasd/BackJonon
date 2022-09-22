cfb43c608d7afa256836fb9cc7a7f363b24dc7fe
woong <dndvud123@naver.com>
2022-09-23 03:26:19 +0900

    sovle the problem

--- 브론즈/브론즈3/2446.py
+++ 브론즈/브론즈3/2446.py
@@ -1,8 +1,8 @@
 n = int(input())
 
 for i in range(n , 0, -1):
-    print(" " * (n - i) + "*" * (i * 2 - 1) + " " * (n - i))
+    print(" " * (n - i) + "*" * (i * 2 - 1))
     
 for j in range(2, n + 1):
-    print(" " * (n - j) + "*" * (j * 2 - 1)+ " " * (n - j))
+    print(" " * (n - j) + "*" * (j * 2 - 1))
     
