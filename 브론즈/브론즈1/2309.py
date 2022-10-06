816e80616be11650b1f5ebf836728851d9b24196
woong <dndvud123@naver.com>
2022-10-06 23:53:49 +0900

    solve the problem

--- 브론즈/브론즈1/2309.py
+++ 브론즈/브론즈1/2309.py
@@ -5,27 +5,18 @@ for _ in range(9):
     tem.append(a)
 
 for i in range(8):
-    s = tem[i]
-    total = sum(tem) - s
-    tem[i] = 0
     for j in range(i + 1, 9):
-        if total - tem[j] == 0:
+        if sum(tem) - tem[i] - tem[j] == 100:
+            tem[i] = 0
             tem[j] = 0
             break
-        else:
-            if total - tem[j] < 100:
-                break
-            
-    if sum(tem) == 0:
+    if sum(tem) == 100:
         break
-    else:
-        tem[i] = s
 
 res = []
-for i in range(9):
-    if tem[i] != 0:
-        res.append(tem[i])
+for x in tem:
+    if x != 0:
+        res.append(x)
 res.sort()
-
 for x in res:
     print(x)
