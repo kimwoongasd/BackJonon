0f741e3f6d0bbd586c1da964cbb6d83ee75d4461
woong <dndvud123@naver.com>
2022-10-11 18:35:16 +0900

    solve the provlem

--- 실버/실버5/1010.py
+++ 실버/실버5/1010.py
@@ -9,3 +9,19 @@ t = int(input())
 for _ in range(t):
     n, m = map(int, input().split())
     print(dp[n][m])
+    
+'''
+def factorial(n):
+    num = 1
+    for i in range(1, n+1):
+        num *= i
+    return num
+
+
+T = int(input())
+
+for _ in range(T):
+    n, m = map(int, input().split())
+    bridge = factorial(m) // (factorial(n) * factorial(m - n))
+    print(bridge)
+'''
