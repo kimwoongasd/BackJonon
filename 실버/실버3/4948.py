633d2f10c7fade4e3675a948923d855ccdf23ceb
woong <dndvud123@naver.com>
2022-11-01 00:25:07 +0900

    시간감소

--- 실버/실버3/4948.py
+++ 실버/실버3/4948.py
@@ -1,30 +1,54 @@
+# 시간 감소
 def f(x):
-    if x == 1:
-        return x
+    count = 0
+    for i in range(x + 1, x * 2 + 1):
+        if ch[i] == 0:
+            count += 1
+    return count
 
-    else:
-        count = 0
-        ch = [0] * (x * 2 + 1)
-        for i in range(2, x * 2 + 1):
-            if ch[i] == 0:
-                for j in range(i * 2, x * 2 + 1, i):
-                    ch[j] = 1
-
-        for i in range(x + 1, x * 2 + 1):
-            if ch[i] == 0:
-                count += 1
-
-        return count
-
-res = []
+ch = [0] * (123456 * 2 + 1)
+ch[0] = 1
+ch[1] = 1
+for i in range(2, 123456 * 2 + 1):
+    if ch[i] == 0:
+        for j in range(i * 2, 123456 * 2 + 1, i):
+            ch[j] = 1
 
 while True:
-    tmp = int(input())
-    if tmp == 0:
+    tem = int(input())
+    if tem == 0:
         break
+    
+    res = f(tem)
+    print(res)
 
-    res.append(f(tmp))
+# def f(x):
+#     if x == 1:
+#         return x
 
-for x in res:
-    print(x)
+#     else:
+#         count = 0
+#         ch = [0] * (x * 2 + 1)
+#         for i in range(2, x * 2 + 1):
+#             if ch[i] == 0:
+#                 for j in range(i * 2, x * 2 + 1, i):
+#                     ch[j] = 1
+
+#         for i in range(x + 1, x * 2 + 1):
+#             if ch[i] == 0:
+#                 count += 1
+
+#         return count
+
+# res = []
+
+# while True:
+#     tmp = int(input())
+#     if tmp == 0:
+#         break
+
+#     res.append(f(tmp))
+
+# for x in res:
+#     print(x)
     
