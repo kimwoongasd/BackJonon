e439560586ea3707f4d1378f63136e92f8992096
woong <dndvud123@naver.com>
2022-11-01 23:54:47 +0900

    solve the problem

---
+++ 실버/실버3/10974.py
@@ -0,0 +1,20 @@
+def dfs(x):
+    if x == n:
+        for x in res:
+            print(x, end=" ")
+        print()
+    else:
+        for i in range(1, n + 1):
+            if ch[i] == 0:
+                res[x] = i
+                ch[i] = 1
+                dfs(x + 1)
+                ch[i] = 0
+        
+        
+n = int(input())
+
+ch = [0] * (n + 1)
+res = [0] * n
+
+dfs(0)
