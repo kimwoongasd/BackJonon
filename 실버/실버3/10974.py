f7d9a5dd5e6472ec42cd6fa93c349fed0ec43c50
woong <dndvud123@naver.com>
2022-11-02 00:09:24 +0900

    solve the problem

--- 실버/실버3/10974.py
+++ 실버/실버3/10974.py
@@ -18,3 +18,12 @@ ch = [0] * (n + 1)
 res = [0] * n
 
 dfs(0)
+
+# 함수사용
+# from itertools import permutations
+
+# n = int(input())
+# ch = [i for i in range(1, n+1)]
+
+# for x in permutations(ch):
+#     print(*x)
