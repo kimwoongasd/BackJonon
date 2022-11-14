ce7891bb0ea638112d8369af32625372a21fb91c
woong <dndvud123@naver.com>
2022-11-14 23:38:59 +0900

    solve the problem

---
+++ 실버/실버2/6603.py
@@ -0,0 +1,24 @@
+def dfs(x, L):
+    global ch, res, n
+    if L == 6:
+        print(*res)
+        
+    else:
+        for i in range(x, n + 1):
+            if ch[i] == 0:
+                res[L] = tem[i]
+                ch[i] = 1
+                dfs(i, L + 1)
+                ch[i] = 0
+
+while True:
+    tem = list(map(int, input().split()))
+    n = tem[0]
+    if tem[0] == 0:
+        break
+    
+    else:
+        ch = [0] * (n + 1)
+        res = [0] * 6
+        dfs(1, 0)
+        print()
